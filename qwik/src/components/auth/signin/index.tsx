a76e5d6ecdbeb20ad6c35b0c776d105f70223475
upayanmazumder <upayanm3@gmail.com>
2024-07-10 21:13:53 +0530

    callbackurl updated

--- qwik/src/components/auth/signin/index.tsx
+++ qwik/src/components/auth/signin/index.tsx
@@ -10,17 +10,17 @@ export default component$(() => {
       <div class={signinstyles.wrapper}>
         <Form action={signIn}>
           <input type="hidden" name="providerId" value="github" />
-          <input type="hidden" name="options.callbackUrl" value="http://localhost:5173/" />
+          <input type="hidden" name="options.callbackUrl" value="https://pmd.upayan.space/api/auth/callback/github" />
           <button class="button button-auth"><BsGithub/></button>
         </Form>
         <Form action={signIn}>
           <input type="hidden" name="providerId" value="google" />
-          <input type="hidden" name="options.callbackUrl" value="http://localhost:5173/" />
+          <input type="hidden" name="options.callbackUrl" value="https://pmd.upayan.space/api/auth/callback/google" />
           <button class="button button-auth"><BsGoogle/></button>
         </Form>
         <Form action={signIn}>
           <input type="hidden" name="providerId" value="discord" />
-          <input type="hidden" name="options.callbackUrl" value="http://localhost:5173/" />
+          <input type="hidden" name="options.callbackUrl" value="https://pmd.upayan.space/api/auth/callback/discord" />
           <button class="button button-auth"><BsDiscord/></button>
         </Form>
       </div>

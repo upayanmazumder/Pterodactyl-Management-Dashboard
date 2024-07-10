d55cbfc1a12e6d40c8c4b4bd38c96d85917e4531
upayanmazumder <upayanm3@gmail.com>
2024-07-10 15:39:50 +0530

    Added google

--- qwik/src/components/auth/signin/index.tsx
+++ qwik/src/components/auth/signin/index.tsx
@@ -1,14 +1,24 @@
 import { component$ } from '@builder.io/qwik';
 import { Form } from '@builder.io/qwik-city';
+import { BsGithub, BsGoogle } from '@qwikest/icons/bootstrap';
 import { useAuthSignin } from '~/routes/plugin@auth';
- 
+import signinstyles from "./signin.module.css"
 export default component$(() => {
   const signIn = useAuthSignin();
   return (
-    <Form action={signIn}>
-      <input type="hidden" name="providerId" value="github" />
-      <input type="hidden" name="options.callbackUrl" value="http://localhost:5173/" />
-      <button>Sign In</button>
-    </Form>
+    <div class="container container-center">
+      <div class={signinstyles.wrapper}>
+        <Form action={signIn}>
+          <input type="hidden" name="providerId" value="github" />
+          <input type="hidden" name="options.callbackUrl" value="http://localhost:5173/" />
+          <button class="button button-auth"><BsGithub/></button>
+        </Form>
+        <Form action={signIn}>
+          <input type="hidden" name="providerId" value="google" />
+          <input type="hidden" name="options.callbackUrl" value="http://localhost:5173/" />
+          <button class="button button-auth"><BsGoogle/></button>
+        </Form>
+      </div>
+    </div>
   );
 });

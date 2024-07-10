8bb980ee428787ff7133e4c0cb242a11fcaace1b
upayanmazumder <upayanm3@gmail.com>
2024-07-10 13:01:18 +0530

    minor fix

--- qwik/src/components/auth/session/session.tsx
+++ qwik/src/components/auth/session/session.tsx
@@ -1,7 +1,9 @@
 import { component$ } from '@builder.io/qwik';
 import { useAuthSession } from '~/routes/plugin@auth';
- 
+
 export default component$(() => {
   const session = useAuthSession();
-  return <p>Logged in with email {session.value?.user?.email}</p>;
+  const loggedInVia = session.value ? `Logged in via ${session.value}` : null;
+
+  return loggedInVia && <p>{loggedInVia}</p>;
 });

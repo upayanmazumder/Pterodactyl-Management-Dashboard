29add019c1d5a8c0f0fd8e7adaa8b04e7b5e5524
upayanmazumder <upayanm3@gmail.com>
2024-07-10 13:13:57 +0530

    uwu

--- qwik/src/components/auth/session/session.tsx
+++ qwik/src/components/auth/session/session.tsx
@@ -1,15 +1,16 @@
 /* eslint-disable qwik/jsx-img */
 import { component$ } from '@builder.io/qwik';
 import { useAuthSession } from '~/routes/plugin@auth';
- 
+
 export default component$(() => {
   const session = useAuthSession();
-  return(
+  const imageUrl = session.value?.user?.image ?? '';
+
+  return (
     <>
-    <p>{session.value?.user?.email}</p>
-    <p>{session.value?.user?.id}</p>
-    <p>{session.value?.user?.name}</p>
-    <p>{session.value?.user?.image}</p>
+      <p>{session.value?.user?.email}</p>
+      <p>{session.value?.user?.name}</p>
+      <img src={imageUrl} alt={session.value?.user?.name ?? 'User Image'} />
     </>
   );
 });

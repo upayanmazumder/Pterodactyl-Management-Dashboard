c534dc3bbc58c5ec3ab1ef77b9d9fe316237c779
upayanmazumder <upayanm3@gmail.com>
2024-07-11 13:15:57 +0530

    uwu server

--- api/server.js
+++ api/server.js
@@ -2,8 +2,17 @@ const admin = require('firebase-admin');
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('./logger');
+const dotenv = require('dotenv');
+const https = require('https');
+const fs = require('fs');
+
 const app = express();
-const port = 3000;
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Access the API_PORT environment variable
+const port = process.env.API_PORT || 3000;
 
 const serviceAccount = require('./firebase/serviceaccountkey.json');
 logger.info(`Server initialised successfully!`);
@@ -60,7 +69,13 @@ app.get('/getUser/:email', async (req, res) => {
   }
 });
 
-// Start the server
-app.listen(port, () => {
-  logger.info(`Server is running on port ${port}`);
+// HTTPS server configuration with .pem files
+const httpsOptions = {
+  key: fs.readFileSync('./api/ssl/key.pem'),
+  cert: fs.readFileSync('./api/ssl/cert.pem')
+};
+
+// Start the HTTPS server
+https.createServer(httpsOptions, app).listen(port, () => {
+  logger.info(`Server is running on HTTPS port ${port}`);
 });

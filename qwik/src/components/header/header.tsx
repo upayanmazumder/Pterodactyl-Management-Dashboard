c1397a4143003ec817c81772d5b085f7eb1b300b
Upayan Mazumder <upayanm3@gmail.com>
2024-07-15 16:41:20 +0530

    🛠️ qwik/src/components/auth/session/session.module.css -> Updated border styles and profile picture dimensions. 🔴 qwik/src/components/header/branding/branding.module.css 🔴 qwik/src/components/header/branding/branding.tsx 🔴 qwik/src/components/header/breadcrumb/breadcrumb.module.css 🔴 qwik/src/components/header/breadcrumb/breadcrumb.tsx 🛠️ qwik/src/components/header/header.module.css -> Added new styles for header and navigation menu. 🛠️ qwik/src/components/header/header.tsx -> Updated header component with new branding and navigation menu. 🔴 qwik/src/components/header/nav/nav.module.css 🔴 qwik/src/components/header/nav/nav.tsx

--- qwik/src/components/header/header.tsx
+++ qwik/src/components/header/header.tsx
@@ -1,20 +1,45 @@
 import { component$ } from "@builder.io/qwik";
 import Session from "../auth/session/session";
-import Branding from "./branding/branding"
-import NavigationMenu from "./nav/nav"
+import Icon from '../../media/icon.png?jsx';
 import headerstyles from "./header.module.css";
-import Breadcrumb from "./breadcrumb/breadcrumb";
+import { useLocation } from "@builder.io/qwik-city";
+
+const menuItems = [
+  { name: 'Home', route: '/' },
+  { name: 'Servers', route: '/servers'},
+  { name: 'Panel', route: 'https://panel.company.com' },
+  { name: 'Admin', route: '/admin/' },
+];
 
 export default component$(() => {
+  const loc = useLocation();
   return (
     <header class={headerstyles.header}>
       <div class={headerstyles.top}>
-        <Branding/>
-        <NavigationMenu/>
+        <div class={headerstyles.branding}>
+          <div class={headerstyles.logo}>
+              <Icon/>
+          </div>
+          <h2 class={headerstyles.text}>
+            Pterodactyl Management Dashboard
+          </h2>
+        </div>
+        <nav class={headerstyles.navMenu}>
+          <ul class={headerstyles.mainMenu}>
+            {menuItems.map((item, index) => (
+                <a key={index} href={item.route} class={headerstyles.link}>
+                  {item.name}
+                </a>
+            ))}
+          </ul>
+        </nav>
         <Session/>
       </div>
       <div class={headerstyles.bottom}>
-        <Breadcrumb />
+        <div class={headerstyles.wrapper}>
+          {loc.isNavigating && <p>Loading...</p>}
+          <p class={headerstyles.pathname}>{loc.url.pathname}</p>
+        </div>
       </div>
     </header>
   );

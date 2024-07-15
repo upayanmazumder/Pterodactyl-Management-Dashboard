cdd5f5c36e69b732cba5b3c7e57b409efd322b4f
Upayan Mazumder <upayanm3@gmail.com>
2024-07-15 21:02:13 +0530

    🛠️ socials.module.css -> Modified styles for social icons. 🛠️ socials.tsx -> Added multiple social media icons dynamically. 🛠️ config.json -> Updated footer and social media links.

--- qwik/src/components/socials/socials.tsx
+++ qwik/src/components/socials/socials.tsx
@@ -1,17 +1,42 @@
+import type { JSX } from "@builder.io/qwik";
 import { component$ } from "@builder.io/qwik";
 import socialstyles from "./socials.module.css";
-import { BsDiscord, BsInstagram } from "@qwikest/icons/bootstrap";
+import { BsDiscord, BsInstagram, BsYoutube, BsTwitter, BsFacebook, BsGitlab, BsGoogle, BsGithub, BsLinkedin, BsPaypal, BsPinterest, BsThreads, BsX, BsWhatsapp, BsCupHot, BsStar } from "@qwikest/icons/bootstrap";
+
+import linksData from '../../data/config.json';
+
+const socialIconMap: { [key: string]: JSX.Element } = {
+  Buymeacoffee: <BsCupHot/>,
+  Discord: <BsDiscord />,
+  Facebook: <BsFacebook />,
+  Github: <BsGithub />,
+  Gitlab: <BsGitlab />,
+  Google: <BsGoogle />,
+  Instagram: <BsInstagram />,
+  Linkedin: <BsLinkedin />,
+  Paypal: <BsPaypal />,
+  Pinterest: <BsPinterest />,
+  Threads: <BsThreads />,
+  Twitter: <BsTwitter />,
+  Trustpilot: <BsStar />,
+  BsWhatsapp: <BsWhatsapp />,
+  X: <BsX />,
+  Youtube: <BsYoutube />,
+  // Add more social icons as needed. Refer to https://icons.getbootstrap.com/?q=social
+};
 
 export default component$(() => {
   return (
     <div class={socialstyles.socialIcons}>
-      <a href="https://discord.com/users/1240025366853193758" class={socialstyles.iconLink} title="Join us on Discord!" target="_blank">
-        <BsDiscord class={socialstyles.icon} />
-      </a>
-
-      <a href="https://www.instagram.com/_._upayan_._/" class={socialstyles.iconLink} title="Follow us on Instagram!" target="_blank">
-        <BsInstagram class={socialstyles.icon} />
-      </a>
+      {linksData.socialLinks.map((social, idx) => {
+        const SocialIcon = socialIconMap[social.name];
+        if (!SocialIcon) return null;
+        return (
+            <a key={idx} href={social.route} class={socialstyles.iconLink} target="_blank">
+              <span class={socialstyles.icon}>{SocialIcon}</span>
+            </a>
+        );
+      })}
     </div>
   );
 });
